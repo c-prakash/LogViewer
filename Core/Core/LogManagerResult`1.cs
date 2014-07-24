0c81edf216252e0544b9a6a560e8bce2406bae5e
c-prakash <cprakash@outlook.com>
2014-07-24 23:25:54 +0100

    Initial version of LogViewer for EntLib
    Initial version of LogViewer for EntLib

---
+++ Core/Core/LogManagerResult`1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Core
+{
+    public class LogManagerResult<T> : LogManagerResult
+    {
+       public LogManagerResult(T result)
+        {
+            Result = result;
+        }
+
+       public LogManagerResult(params string[] errors)
+            : base(errors)
+        {
+        }
+
+        public T Result { get; private set; }
+    }
+}
---
+++ Core/Core/LogResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Core.Core
+{
+    public class LogResult
+    {
+        public int LogID { get; set; }
+        public int? EventID { get; set; }
+        public int Priority { get; set; }
+        public string Severity { get; set; }
+        public string Title { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string MachineName { get; set; }
+        public string AppDomainName { get; set; }
+        public string ProcessID { get; set; }
+        public string ProcessName { get; set; }
+        public string ThreadName { get; set; }
+        public string Win32ThreadId { get; set; }
+        public string Message { get; set; } 
+        public string FormattedMessage { get; set; }
+    }
+}
